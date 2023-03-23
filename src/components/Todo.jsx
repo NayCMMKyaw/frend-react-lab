63c32440f5b9e63ec91582843f65815976805b09
naychekyaw <76429445+naychekyaw@users.noreply.github.com>
2023-03-23 23:35:59 +0700

    Checkpt1 commit

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,71 @@
 import { useState } from "react";
 import CardList from "./CardList";
+import {Box, Typography, Modal, Textfield, Button} from "@mui/material";
+import { bgcolor } from "@mui/system";
+
+const addNewTaskBarStyle = {
+  padding: "20px",
+  background: "#fefffe",
+  width: { xs: "300px", md:"500px"},
+  borderRadius: "10px",
+  margin: "20px 0px",
+  fontSize: "25px",
+  color: "#7b7b7b",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+};
+
+const wrapperTodoListStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  marginTop: "20px",
+};
+
+const wrapperHeaderStyle ={
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+};
+
+const headerTextStyle = {
+  color: "#fefffb",
+  fontSize: { xs: "50px", md: "60px"},
+};
+
+const headerTodoListLengthStyle = {
+  padding: "20px 30px",
+  backgroundColor: "#b0a3f5",
+  boxShadow: " 0 4px 8px 0 rgba(0,0,0,0.2)",
+  fontSize: "50px",
+  borderRadius: "10px",
+  color: "white",
+  fontWeight: "bold",
+};
+
+const modalStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+  borderRadius: "10px",
+};
+
+const horizontalStyle = {
+  width: "90%",
+  backgroundColor: "white",
+  boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+};
 
 function Todo() {
   const [todo, setTodo] = useState([]);
@@ -9,48 +75,25 @@ function Todo() {
     setTodo([...todo, "new task"])
   }
   return (
-    <div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
+    <Box sx={{ margin: "20px" }}>
+      <div style={wrapperHeaderStyle}>
         <div>
-          <h1 style={{ color: "#fefffb", fontSize: "60px" }}>Incoming</h1>
+          <Typography variant="h1" sx={headerTextStyle}>
+            Incoming
+          </Typography>
         </div>
-        <div
-          style={{
-            padding: "20px 30px",
-            backgroundColor: "#b0a3f5",
-            fontSize: "50px",
-            borderRadius: "10px",
-            color: "white",
-            fontWeight: "bold",
-          }}
-        >
-          {todo.length}
-        </div>
+        <div style={headerTodoListLengthStyle}>{todo.length}</div>
       </div>
-      <div
-        style={{
-          padding: "20px",
-          background: "#fefffe",
-          width: "500px",
-          borderRadius: "10px",
-          margin: "20px 0px",
-          fontSize: "25px",
-          color: "#7b7b7b",
-        }}
-        onClick={handleAdd}
-      >
+      <Box sx={wrapperTodoListStyle}>
+        <Box sx={addNewTaskBarStyle} onClick={handleAdd}>
         + Add New Tasks
-      </div>
+      </Box>
       {todo.map((item)=>{
         return <CardList task={item}/>
       })}
-    </div>
+      </Box>
+      <hr style={horizontalStyle} />
+    </Box>
   );
 }
 export default Todo;
